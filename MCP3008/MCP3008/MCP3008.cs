5806f7f896e8f499339a9f6a50232b85567a8df4
David Shoemaker <David.Shoemaker@microsoft.com>
2015-08-20 10:42:51 -0700

    Prototype done, reading all 8 channels

--- MCP3008/MCP3008/MCP3008.cs
+++ MCP3008/MCP3008/MCP3008.cs
@@ -12,16 +12,19 @@ namespace MCP3008
     class MCP3008
     {
         private SpiDevice mcp3008;
-        const int SPI_CHIP_SELECT_LINE = 24;
+        const int SPI_CHIP_SELECT_LINE = 0;  // SPI0 CS0 pin 24
+        const byte MCP3008_SingleEnded = 0x08;
+        const byte MCP3008_Differential = 0x00;
+
         public async void Initialize()
         {
             try
             {
                 var settings = new SpiConnectionSettings(SPI_CHIP_SELECT_LINE);
                 settings.ClockFrequency = 3600000;                              // 3.6MHz is the rated speed of the MCP3008 at 5v
-                settings.Mode = SpiMode.Mode3;                                  /* The chip expects an idle-high clock polarity, we use Mode3    
-                                                                                * to set the clock polarity and phase to: CPOL = 1, CPHA = 1         
-                                                                            */
+                settings.Mode = SpiMode.Mode1;                               
+                                                                               
+                                                                               
 
                 string aqs = SpiDevice.GetDeviceSelector();                     /* Get a selector string that will return all SPI controllers on the system */
                 var dis = await DeviceInformation.FindAllAsync(aqs);            /* Find the SPI bus controller devices with our selector string             */
@@ -48,29 +51,45 @@ namespace MCP3008
         private async Task begin()
         {
             Debug.WriteLine("TCS34725::begin");
-            byte[] WriteBuffer = new byte[] { TCS34725_ID | TCS34725_COMMAND_BIT };
-            byte[] ReadBuffer = new byte[] { 0xFF };
 
-            // Check the device signature
-            mcp3008.WriteRead(WriteBuffer, ReadBuffer);
-            Debug.WriteLine("TCS34725 Signature: " + ReadBuffer[0].ToString());
-
-            if (ReadBuffer[0] != 0x44)
-            {
-                Debug.WriteLine("TCS34725::begin Signature MISMATCH!!!!!!!!!!!!!!!!!");
-                return;
-            }
+            await Task.Delay(5);
 
             Init = true;
-
         }
 
-
-
-        public int ReadADC(int whichChannel)
+        public async Task<int> ReadADC(byte whichChannel)
         {
+            // To line everything up for ease of reading back (on byte boundarys) we 
+            // will pad the command start bit with 7 leading "0" bits
 
-            return 0;
+            // Write 0000 000S GDDD xxxx xxxx xxxx
+            // Read  ???? ???? ???? ?N98 7654 3210
+            // S = start bit
+            // G = Single / Differentail
+            // D = chanel data 
+            // ? = undefined, ignore
+            // N = 0 "Null bit"
+            // 9-0 = 10 data bits
+
+            if (!Init) await begin();
+
+            byte command = whichChannel;
+            command |= MCP3008_SingleEnded;
+            command <<= 4;
+
+            byte[] commandBuf = new byte[] { 0x01, command, 0x00};
+
+            byte[] readBuf = new byte[] { 0x00, 0x00, 0x00};
+
+            mcp3008.TransferFullDuplex(commandBuf, readBuf);
+
+            int sample = readBuf[2] + ((readBuf[1] & 0x03) << 8);
+            int s2 = sample & 0x3FF;
+            Debug.Assert(sample == s2);
+
+            Debug.WriteLine("MCP3008::ReadADC C:{0} {1}", whichChannel, sample.ToString());
+
+            return sample;
         }
     }
 }
--- MCP3008/MCP3008/MainPage.xaml.cs
+++ MCP3008/MCP3008/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,25 @@ namespace MCP3008
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        MCP3008 mcp3008 = new MCP3008();
         public MainPage()
         {
             this.InitializeComponent();
+
+            mcp3008.Initialize();
+        }
+
+        private async void OnReadClick(object sender, RoutedEventArgs e)
+        {
+            for (byte adc = 0; adc < 8; adc++)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    int readVal = await mcp3008.ReadADC(adc);
+                    Debug.WriteLine("Read value: " + readVal.ToString());
+                    await Task.Delay(100);
+                }
+            }
         }
     }
 }
