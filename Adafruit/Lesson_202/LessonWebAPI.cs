cabe8501e8651a8ed14d0854725945f9d1c3b6b5
David Shoemaker <David.Shoemaker@microsoft.com>
2015-09-11 10:32:02 -0700

    Lesson 202 first draft

---
+++ Adafruit/Lesson_202/LessonWebAPI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_202
+{
+    static public class LessonWebAPI
+    {
+        const string WebAPIURL = "http://adafruitsample.azurewebsites.net/api?Lesson202=1";
+
+        // This will call an exposed web API at the indicated URL, this will seed the 
+        // maker map with an approximation of your location on the world map.
+        //
+        // See: http://adafruitsample.azurewebsites.net/SamplesMap.html for your pin!
+        //
+        // The API will return the current time as a string.
+        // Example return: "2015-08-31T21:56:25.766Z"
+        static public async Task<string> MakeLessonWebApiCall()
+        {
+            Debug.WriteLine("LessonWebAPI::MakeLessonWebApiCall");
+
+            string responseString = "No response";
+            try
+            {
+                // Prepare the web API call
+                WebRequest request = WebRequest.Create(WebAPIURL);
+
+                // Wait for the response notification
+                WebResponse response = await request.GetResponseAsync();
+
+                // Read the response string into memory.
+                StreamReader streamReader = new StreamReader(response.GetResponseStream());
+                responseString = streamReader.ReadToEnd();
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+
+            return responseString;
+        }
+    }
---
+++ Adafruit/Lesson_202/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Lesson_202
+{
+    /// <summary>
+    /// The application main page.  Because we are running headless we will not see anything
+    /// even though it is begin generated at runtime.  This acts as the main entry point for the 
+    /// application functionality.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // The class which wraps our ADC chip.
+        MCP3008 mcp3008;
+
+        // The voltage we are using as the ADC reference voltage
+        const float ADCReferenceVoltage = 3.3F;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // This method will be called by the application framework when the page is first loaded.
+        protected override async void OnNavigatedTo(NavigationEventArgs navArgs)
+        {
+            Debug.WriteLine("MainPage::OnNavigatedTo");
+
+            await LessonWebAPI.MakeLessonWebApiCall();
+
+            try
+            {
+                // Create a new InternetLed object
+                mcp3008 = new MCP3008(ADCReferenceVoltage);
+
+                // Initialize it for use
+                mcp3008.Initialize();
+
+                byte whichChannel = 0;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    // Read the first adc channel, this will get back an ADC value between MCP3008.MIN (0) and MCP3008.MAX (1023)
+                    int adcValue = await mcp3008.ReadADC(whichChannel);
+
+                    // The convert that to a voltage
+                    float voltage = mcp3008.ADCToVoltage(adcValue);
+
+                    Debug.WriteLine(String.Format("Channel: {0}, ADC Value: {1}, Voltage: {2}", whichChannel, adcValue, voltage));
+
+                    // Wait 1 second between loops.
+                    await Task.Delay(1000);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
